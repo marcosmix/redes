01fe6dced6d7045c227cd23190a32eabba3aaa46
Marcos Caballero <madc_801@hotmail.com>
2022-02-08 09:08:11 -0300

    fecha y url

--- app/Http/Controllers/Tratamiento.php
+++ app/Http/Controllers/Tratamiento.php
@@ -23,22 +23,38 @@ class Tratamiento extends Controller{
         return $hashtac;
     }
 
+    static public function mm_numero($mes)
+    {
+        switch($mes)
+        {
+            
+        }
+    }
+    static public function FormateoFecha($fecha)
+    {
+        $fecha_array=explode(' ',$fecha);
+        $Fecha_DDMMAAAA=[$fecha_array[2],$fecha_array[1],$fecha_array[5]];
+        var_dump($Fecha_DDMMAAAA);
+        
+        die;
+    }
     static public function ConsultaBusquedaTwitter($consulta)
     {
         $consulta_tratada=array();
-  
+        
+
         foreach($consulta->statuses as $tw)
         {
-            
+           // var_dump($tw->entities->urls[0]->url);die;
           
             $twitt=[
                 'id_twitt' => $tw->id,
                 'id_twitt' => $tw->user->id,
-                'url' => "www.twitter.com",
+                'url' => $tw->entities->urls[0]->url,
                 'cuerpo' => $tw->text,
                 //'menciones' => $tw->menciones,
                 'hashtag' =>Tratamiento::Hashctac($tw->entities->hashtags),
-                'fecha' => $tw->created_at,
+                'fecha' =>Tratamiento::FormateoFecha($tw->created_at),
                 'menciones' =>"usuario mencionado",
                 'id_str' => $tw->id_str,
                 'name' => $tw->user->name,
