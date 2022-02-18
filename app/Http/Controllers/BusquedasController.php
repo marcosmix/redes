4043fc7ad9b3f758a7ca8af987dc5ed83a6e3081
Marcos Caballero <madc_801@hotmail.com>
2022-02-18 11:25:11 -0300

    fechas futuro

--- app/Http/Controllers/BusquedasController.php
+++ app/Http/Controllers/BusquedasController.php
@@ -48,15 +48,15 @@ class BusquedasController extends Controller
         $fecha = date("Y-m-d");
         $resultados = array();
         $resultadosUnificados = array();
-       var_dump($fecha);
+       var_dump(date("Y-m-d", strtotime($fecha . "+1days")));
        
-       //array_push($resultados, $connection->get("search/tweets", ['q' => $query, 'count' => $cantPorFecha, 'exclude_replies' => true, 'lang' => 'es', 'until' => $fecha]));
-        for ($i = 1; $i < $D; $i++)
-        var_dump(date("Y-m-d", strtotime($fecha . "-" . $i . "days")));
-
-        die;
-        // array_push($resultados, $connection->get("search/tweets", ['q' => $query, 'count' => $cantPorFecha, 'exclude_replies' => true, 'lang' => 'es', 'until' => date("Y-m-d", strtotime($fecha . "-" . $i . "days"))]));
-
+       array_push($resultados, $connection->get("search/tweets", ['q' => $query, 'count' => $cantPorFecha, 'exclude_replies' => true, 'lang' => 'es', 'until' => date("Y-m-d", strtotime($fecha . "+1days"))]));
+        for ($i = 0; $i < $D-1; $i++)
+        {
+            var_dump(date("Y-m-d", strtotime($fecha . "-" . $i . "days")));
+            array_push($resultados, $connection->get("search/tweets", ['q' => $query, 'count' => $cantPorFecha, 'exclude_replies' => true, 'lang' => 'es', 'until' => date("Y-m-d", strtotime($fecha . "-" . $i . "days"))]));
+        }
+        
         foreach ($resultados as $r)
             $r->statuses = $this->OrdenarResultadosTwitter($r->statuses);
 
