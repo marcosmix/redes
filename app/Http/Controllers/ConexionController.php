c449cc2320460b63b165f0ad39e854b4167f35ad
Marcos <46447332+marcosmix@users.noreply.github.com>
2022-09-09 11:47:59 -0300

    final beta

--- app/Http/Controllers/ConexionController.php
+++ app/Http/Controllers/ConexionController.php
@@ -8,16 +8,12 @@ class ConexionController extends Controller{
 
     static public function ConectarTwitter()
     {
-        $consumer_api_key = 'RcPrp0xpAkFo0f9VUMVedxA43';
-        $consumer_api_key_secret = '70hfrTBDwGtPsXg6ul4aK282486ADuxHVn1YfALs8FDITFTVfK';
-        $authentication_tokens_bearer = 'AAAAAAAAAAAAAAAAAAAAAA%2FuYgEAAAAA3Zr%2Bs8TGkhayI6SnttL4qw4J6GE%3DkZuvjPCurOMbCJ4eAhaKEGaiZVmlqLvQtcOkAm3SeeiJb98BTn';
-        $access_token = '1427480618661728256-jauOdcazJtrP8E5d0viIkkfFGWiL5J';
-        $access_token_secret = 'ZyQIcWc9u0exilBdWep0xCZldyzCquJB61bGeV8g8DjRY';
+       
         $connection = new TwitterOAuth(
-            $consumer_api_key,
-            $consumer_api_key_secret,
-            $access_token,
-            $access_token_secret
+            env('consumer_api_key'),
+            env('consumer_api_key_secret'),
+            env('access_token'),
+            env('access_token_secre')
         );
 
         return $connection;
